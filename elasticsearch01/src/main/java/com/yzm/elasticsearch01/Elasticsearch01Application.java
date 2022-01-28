e06ef0ba5ac45e367a491c94f767be1cb318750b
yezhimincxvxb <395995486@qq.com>
2022-01-28 14:16:33 +0800

    es

---
+++ elasticsearch01/src/main/java/com/yzm/elasticsearch01/Elasticsearch01Application.java
@@ -0,0 +1,13 @@
+package com.yzm.elasticsearch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elasticsearch01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Elasticsearch01Application.class, args);
+    }
+
+}
---
+++ elasticsearch01/src/test/java/com/yzm/elasticsearch01/Elasticsearch01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzm.elasticsearch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elasticsearch01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
